2154f24811616e489fe9e86911970d41d808990a
12301031-Chen Jixuan <12301031@bjtu.edu.cn>
2015-04-01 00:16:04 +0800

    Express Assistant

---
+++ app/src/main/java/com/team20/java/expressassistant1/HistoryDetail.java
@@ -0,0 +1,68 @@
+package com.team20.java.expressassistant1;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class HistoryDetail extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private int itemNum;
+    private static final String ARG_PARAM1=null;
+
+    // TODO: Rename and change types and number of parameters
+    public static HistoryDetail newInstance(int position) {
+        HistoryDetail fragment = new HistoryDetail();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, position);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public HistoryDetail() {
+        // Required empty public constructor
+    }
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment SecondFragment.
+     */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            itemNum = getArguments().getInt(ARG_PARAM1)+1;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_history_detail, container, false);
+        TextView textView2 = (TextView)view.findViewById(R.id.textView2);
+        textView2.setText("Item " + String.valueOf(itemNum) + " is clicked.");
+        return view;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
